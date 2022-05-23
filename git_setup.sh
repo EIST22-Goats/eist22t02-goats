c481d8c4577a8e3710f05f3ad17b65e4d69cec0a
Aamin Gem <gem.aamin@gmail.com>
2022-05-23 13:16:15 +0200

    Create git_setup.sh

---
+++ git_setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo git config --system receive.denyNonFastForwards true && git remote add bitbucket ssh://git@bitbucket.ase.in.tum.de:7999/eist22t02/eist22t02-goats.git && git config --global alias.pushall '!git remote | xargs -L1 git push --all'
