7f620b081fbf731998f9a672a02f0913bef473a1
FlorianNAdam <nalu.adam@yahoo.com>
2022-06-16 23:08:07 +0200

    preload elements with id

--- src/main/resources/static/js/appointmentModals.js
+++ src/main/resources/static/js/appointmentModals.js
@@ -1,16 +1,34 @@
 const deleteAppointmentModalForm = $("#deleteAppointmentModalForm");
+
 const changeDateModalForm = $("#changeDateModalForm");
 const changeButton = $("#appointmentModalChangeBtn")
+const appointmentDataColumn = $("#appointmentDataColumn")
 const changeAppointmentModalNameInput = $("#changeAppointmentModalNameInput")
 const changeAppointmentModalDescriptionInput = $("#changeAppointmentModalDescriptionInput")
 const changeAppointmentModalStartDateInput = $("#changeAppointmentModalStartDateInput")
+const changeAppointmentModalStartTimeInput = $("#changeAppointmentModalStartTimeInput")
+const changeAppointmentModalEndTimeInput = $("#changeAppointmentModalEndTimeInput")
+const changeAppointmentModalRepetitionsInput = $("#changeAppointmentModalRepetitionsInput")
+const changeAppointmentModalDeleteBtn = $("#changeAppointmentModalDeleteBtn")
+
 const locationView = $("#locationView");
+const locationViewDiv = $("#locationViewDiv");
 const roomView = $("#roomView");
+const appointmentName = $("#appointmentName");
+const appointmentDescription = $("#appointmentDescription")
+const appointmentStartDate = $("#appointmentStartDate")
+const appointmentEndTime = $("#appointmentEndTime")
+const appointmentStartTime = $("#appointmentStartTime")
+const appointmentRepetitions = $("#appointmentRepetitions")
+const appointmentRoomName = $("#appointmentRoomName")
+const appointmentRoomNameDiv = $("#appointmentRoomNameDiv")
+const appointmentAddress = $("#appointmentAddress")
+const appointmentAddressDiv = $("#appointmentAddressDiv")
 
 const deleteAppointmentModal = $('#deleteAppointmentModal')
 const changeAppointmentModal = $('#changeDateModal')
 const createAppointmentModal = $('#newDateModal')
-const appointmentModal = $('#timeslotModal')
+const appointmentModal = $('#appointmentModal')
 
 function loadDeleteAppointmentModal(modal) {
     modal.on('show.bs.modal', function (event) {
@@ -102,13 +120,13 @@ function loadChangeAppointmentModal(modal) {
         changeAppointmentModalStartDateInput.val(startDate);
 
         let startTime = event.relatedTarget.getAttribute('data-bs-starttime');
-        $("#changeAppointmentModalStartTimeInput").val(startTime);
+        changeAppointmentModalStartTimeInput.val(startTime);
 
         let endTime = event.relatedTarget.getAttribute('data-bs-endtime');
-        $("#changeAppointmentModalEndTimeInput").val(endTime);
+        changeAppointmentModalEndTimeInput.val(endTime);
 
         let repetitions = event.relatedTarget.getAttribute('data-bs-repetitions');
-        $("#changeAppointmentModalRepetitionsInput").val(repetitions);
+        changeAppointmentModalRepetitionsInput.val(repetitions);
 
         let address = event.relatedTarget.getAttribute("data-bs-address");
         let addressInput = $(modal).find(".address-input");
@@ -119,8 +137,7 @@ function loadChangeAppointmentModal(modal) {
         roomNameInput.val(roomName);
 
         let appointmentId = event.relatedTarget.getAttribute("data-bs-appointment-id");
-        let deleteButton = modal.find("#changeAppointmentModalDeleteBtn");
-        deleteButton.attr("data-bs-appointment-id", appointmentId)
+        changeAppointmentModalDeleteBtn.attr("data-bs-appointment-id", appointmentId)
 
         changeDateModalForm.attr('action', "/updateAppointment/" + appointmentId);
 
@@ -159,57 +176,57 @@ function loadAppointmentModal(modal) {
 
         let name = event.relatedTarget.getAttribute('data-bs-name')
         changeButton.attr('data-bs-name', name)
-        $("#appointmentName").text(name);
+        appointmentName.text(name);
 
         let description = event.relatedTarget.getAttribute('data-bs-description')
         changeButton.attr('data-bs-description', description)
-        $("#appointmentDescription").text(description);
+        appointmentDescription.text(description);
 
         let startDate = event.relatedTarget.getAttribute('data-bs-startdate');
         changeButton.attr('data-bs-startdate', startDate)
-        $("#appointmentStartDate").text(startDate);
+        appointmentStartDate.text(startDate);
 
         let startTime = event.relatedTarget.getAttribute('data-bs-starttime');
         changeButton.attr('data-bs-starttime', startTime)
-        $("#appointmentStartTime").text(startTime);
+        appointmentStartTime.text(startTime);
 
         let endTime = event.relatedTarget.getAttribute('data-bs-endtime');
         changeButton.attr('data-bs-endtime', endTime)
-        $("#appointmentEndTime").text(endTime);
+        appointmentEndTime.text(endTime);
 
         let repetitions = event.relatedTarget.getAttribute('data-bs-repetitions');
         changeButton.attr('data-bs-repetitions', repetitions)
-        $("#appointmentRepetitions").text(repetitions);
+        appointmentRepetitions.text(repetitions);
 
         let roomName = event.relatedTarget.getAttribute('data-bs-roomname');
         changeButton.attr('data-bs-roomname', roomName)
-        $("#appointmentRoomName").text(roomName);
+        appointmentRoomName.text(roomName);
 
         if (roomName === null) {
-            $("#appointmentRoomNameDiv").addClass("d-none");
+            appointmentRoomNameDiv.addClass("d-none");
             roomView.addClass("d-none");
         } else {
-            $("#appointmentRoomNameDiv").removeClass("d-none");
+            appointmentRoomNameDiv.removeClass("d-none");
             roomView.removeClass("d-none");
         }
 
         let address = event.relatedTarget.getAttribute('data-bs-address');
         changeButton.attr('data-bs-address', address)
-        $("#appointmentAddress").text(address);
+        appointmentAddress.text(address);
         if (address === null) {
-            $("#appointmentAddressDiv").addClass("d-none");
+            appointmentAddressDiv.addClass("d-none");
         } else {
-            $("#appointmentAddressDiv").removeClass("d-none");
+            appointmentAddressDiv.removeClass("d-none");
         }
 
         if (address !== null || roomName !== null) {
-            $("#timeslotModal").addClass("modal-lg");
-            $("#appointmentDataColumn").addClass("col-4");
-            $("#locationViewDiv").removeClass("d-none");
+            appointmentModal.addClass("modal-lg");
+            appointmentDataColumn.addClass("col-4");
+            locationViewDiv.removeClass("d-none");
         } else {
-            $("#timeslotModal").removeClass("modal-lg");
-            $("#appointmentDataColumn").removeClass("col-4");
-            $("#locationViewDiv").addClass("d-none");
+            appointmentModal.removeClass("modal-lg");
+            appointmentDataColumn.removeClass("col-4");
+            locationViewDiv.addClass("d-none");
         }
 
         setTimeout(() => {
